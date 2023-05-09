b199e3082296ca77059f0add8e97b4debdcdae56
cosmingherghe <cosminlinux@gmail.com>
2023-05-08 22:20:19 -0400

    GridPane - Constraints

--- layouts/src/main/java/dev/cosmingherghe/layouts/Main.java
+++ layouts/src/main/java/dev/cosmingherghe/layouts/Main.java
@@ -18,34 +18,35 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         GridPane pane = new GridPane();
-//        pane.setGridLinesVisible(true);
+        pane.setGridLinesVisible(true);
         pane.setHgap(10);
         pane.setVgap(10);
         pane.requestFocus();
         pane.setAlignment(Pos.CENTER);
 
-        Button b1 = new Button("1");
-        Button b2 = new Button("2");
-        Button b3 = new Button("3");
+        for (int col = 0; col < 3; col++) {
+            for (int row = 0; row < 3; row++) {
+                Button btn = new Button(col + "|" + row);
+                pane.add(btn, col, row);
+            }
+        }
 
-        Label label = new Label("A rectagnle");
+        ColumnConstraints cc1 = new ColumnConstraints(100);
+        cc1.setHalignment(HPos.LEFT);
+        ColumnConstraints cc2 = new ColumnConstraints(200);
+        cc2.setHalignment(HPos.CENTER);
+        ColumnConstraints cc3 = new ColumnConstraints(200);
+        cc3.setHalignment(HPos.RIGHT);
 
-//        1ST Way to add elements to the panel
-//        GridPane.setConstraints(b1, 0, 0);
-//        GridPane.setConstraints(b2, 1, 0);
-//        GridPane.setConstraints(b3, 1, 1);
-//        GridPane.setConstraints(label, 0, 1);
-//        pane.getChildren().addAll(b1, b2, b3, label);
+        RowConstraints rc1 = new RowConstraints(50);
+        rc1.setValignment(VPos.TOP);
+        RowConstraints rc2 = new RowConstraints(70);
+        rc2.setValignment(VPos.CENTER);
+        RowConstraints rc3 = new RowConstraints(60);
+        rc3.setValignment(VPos.CENTER);
 
-//        2ND Way of adding elements together
-//        pane.add(b1, 0, 0);
-//        pane.add(b2, 1, 0);
-//        pane.add(b3, 1, 1);
-//        pane.add(label, 0, 1);
-
-//        3RD Way
-          pane.addRow(0, b1, b2, b3);
-          pane.add(label, 0,1, GridPane.REMAINING, 1);
+        pane.getColumnConstraints().addAll(cc1,cc2,cc3);
+        pane.getRowConstraints().addAll(rc1,rc2,rc3);
 
         Scene scene = new Scene(pane, 800, 800);
         stage.setTitle("BorderPane");
