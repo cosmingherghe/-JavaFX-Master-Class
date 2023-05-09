16a1b611d1e6a375f861f962fa08c503ed18a550
cosmingherghe <cosminlinux@gmail.com>
2023-05-08 23:46:27 -0400

    TextFlow

--- layouts/src/main/java/dev/cosmingherghe/layouts/Main.java
+++ layouts/src/main/java/dev/cosmingherghe/layouts/Main.java
@@ -10,6 +10,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,18 +18,14 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        AnchorPane pane = new AnchorPane();
-        Button bottomRight = new Button("Bottom Right");
-        Button topLeft = new Button("Top Left");
-
-        AnchorPane.setBottomAnchor(bottomRight, 10.0);
-        AnchorPane.setRightAnchor(bottomRight, 10.0);
-
-        AnchorPane.setTopAnchor(topLeft, 10.0);
-        AnchorPane.setLeftAnchor(topLeft, 10.0);
-
-        pane.getChildren().addAll(topLeft, bottomRight);
-
+        TextFlow pane = new TextFlow();
+        Text heading = new Text("What is a paragraph?");
+        heading.setFill(Color.GREEN);
+        heading.setFont(Font.font("Arial", FontWeight.BOLD, 19));
+        Text paragraph = new Text("\nParagraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.");
+        pane.getChildren().addAll(heading, paragraph);
+        pane.setLineSpacing(5);
+        pane.setTextAlignment(TextAlignment.CENTER);
         Scene scene = new Scene(pane, 800, 800);
         stage.setTitle("AnchorPane");
         stage.setScene(scene);
