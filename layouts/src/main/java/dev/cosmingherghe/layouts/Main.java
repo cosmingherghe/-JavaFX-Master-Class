57b77240fb41b311a8901fb0d148effec34e40b5
cosmingherghe <cosminlinux@gmail.com>
2023-05-08 22:31:38 -0400

    AnchorPane

--- layouts/src/main/java/dev/cosmingherghe/layouts/Main.java
+++ layouts/src/main/java/dev/cosmingherghe/layouts/Main.java
@@ -17,39 +17,20 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        GridPane pane = new GridPane();
-        pane.setGridLinesVisible(true);
-        pane.setHgap(10);
-        pane.setVgap(10);
-        pane.requestFocus();
-        pane.setAlignment(Pos.CENTER);
+        AnchorPane pane = new AnchorPane();
+        Button bottomRight = new Button("Bottom Right");
+        Button topLeft = new Button("Top Left");
 
-        for (int col = 0; col < 3; col++) {
-            for (int row = 0; row < 3; row++) {
-                Button btn = new Button(col + "|" + row);
-                pane.add(btn, col, row);
-            }
-        }
+        AnchorPane.setBottomAnchor(bottomRight, 10.0);
+        AnchorPane.setRightAnchor(bottomRight, 10.0);
 
-        ColumnConstraints cc1 = new ColumnConstraints(100);
-        cc1.setHalignment(HPos.LEFT);
-        ColumnConstraints cc2 = new ColumnConstraints(200);
-        cc2.setHalignment(HPos.CENTER);
-        ColumnConstraints cc3 = new ColumnConstraints(200);
-        cc3.setHalignment(HPos.RIGHT);
+        AnchorPane.setTopAnchor(topLeft, 10.0);
+        AnchorPane.setLeftAnchor(topLeft, 10.0);
 
-        RowConstraints rc1 = new RowConstraints(50);
-        rc1.setValignment(VPos.TOP);
-        RowConstraints rc2 = new RowConstraints(70);
-        rc2.setValignment(VPos.CENTER);
-        RowConstraints rc3 = new RowConstraints(60);
-        rc3.setValignment(VPos.CENTER);
-
-        pane.getColumnConstraints().addAll(cc1,cc2,cc3);
-        pane.getRowConstraints().addAll(rc1,rc2,rc3);
+        pane.getChildren().addAll(topLeft, bottomRight);
 
         Scene scene = new Scene(pane, 800, 800);
-        stage.setTitle("BorderPane");
+        stage.setTitle("AnchorPane");
         stage.setScene(scene);
         stage.show();
     }
